2e933bcac797bb11c2d0a9cd3affbf73563d5aad
karthik bandla <kbandla@pdx.edu>
2025-02-16 18:06:33 -0800

    optimized dumping into memory space code using StoreMem and ReadMem Functions

--- RISCV.cpp
+++ RISCV.cpp
@@ -24,13 +24,6 @@ std::string MemoryImage;
 int CurrentInstr;
 
 
-void Fetch(){
-
-    CurrentInstr = MemorySpace[pc] | (MemorySpace[pc + 1] << 8) | (MemorySpace[pc + 2] << 16) | (MemorySpace[pc + 3] << 24);
-    #ifdef debug
-    std::cout << "Current Instruction: " <<std::hex<< CurrentInstr << std::endl;
-    #endif
-}
 
 
 int ReadMem(int pc, int datatype, int sign)
@@ -38,7 +31,7 @@ int ReadMem(int pc, int datatype, int sign)
 
     int mem;
     if ((datatype == byte) && (sign==negative))
-       mem=MemorySpace[pc]>>7?MemorySpace[pc]|(0xffffff00):MemorySpace[4];
+       mem=MemorySpace[pc]>>7?MemorySpace[pc]|(0xffffff00):MemorySpace[pc];
 
     else if (datatype == byte)
        mem=MemorySpace[pc];
@@ -57,7 +50,8 @@ int ReadMem(int pc, int datatype, int sign)
 }
 
 
- void StoreMem(int location, int bytes, int regst){
+ void StoreMem(int location, int bytes, int regst)
+ {
 
     for(int i=0;i<bytes;i++)
     {
@@ -65,12 +59,29 @@ int ReadMem(int pc, int datatype, int sign)
     }
     
 
+ }
+
+
+void Fetch()
+{
+
+    CurrentInstr = ReadMem(pc, word, positive);
+    #ifdef debug
+    std::cout << "Current Instruction: " <<std::hex<< CurrentInstr << std::endl;
+    #endif
 }
 
-int main(int argc, char* argv[]) {
+// void Decode();
+// void Execute();
+
+
+
+int main(int argc, char* argv[]) 
+{
     int ProgramSize = 0;
 
-    switch(argc){
+    switch(argc)
+    {
         case 4: MemoryImage = argv[1];
                 StartAddress = std::stoi(argv[2]);
                 StackAddress = std::stoi(argv[3]);
@@ -83,28 +94,28 @@ int main(int argc, char* argv[]) {
 
     }
 
-    if(argc==1){
-    }
-
     #ifdef debug
     std::cout << "StartAddress: " << StartAddress << "\n";
     std::cout << "StackAddress: " << StackAddress << "\n";
     #endif
 
     std::ifstream file(MemoryImage);
-    if (!file) {
+    if (!file) 
+    {
         std::cerr << "Error: Could not open file!" << std::endl;
         return 0;
     }
 
     std::string line;
-    while (std::getline(file, line)) {
+    while (std::getline(file, line)) 
+    {
         std::istringstream stream(line);
         uint32_t address, value;
         char colon;
         std::string data;
 
-        if (stream >> std::hex >> address >> colon >> data) {
+        if (stream >> std::hex >> address >> colon >> data) 
+        {
             #ifdef debug
             std::cout << "Address: 0x" << std::hex << address
                       << " => data: " << data << std::endl;
@@ -113,22 +124,9 @@ int main(int argc, char* argv[]) {
             sscanf(data.c_str(), "%x", &value);
             ProgramSize += 4; 
 
-            if (data.size() == 8) 
-            {
-                MemorySpace[address]   = value & 0xFF;
-                MemorySpace[address + 1] = (value >> 8) & 0xFF;
-                MemorySpace[address + 2] = (value >> 16) & 0xFF;
-                MemorySpace[address + 3] = (value >> 24) & 0xFF;
-            } 
-            else if (data.size() == 4) 
-            {
-                MemorySpace[address]   = value & 0xFF;
-                MemorySpace[address + 1] = (value >> 8) & 0xFF;
-            } 
-            else if (data.size() == 2) 
-            {
-                MemorySpace[address]   = value & 0xFF;
-            }
+
+            StoreMem(address, (data.size())/2, value );    //Storing into Memory Space
+
         }
     }
     file.close();
@@ -137,110 +135,25 @@ int main(int argc, char* argv[]) {
 
     #ifdef debug
     std::cout << "\nMemory Contents:\n";
-    for (int i = 0; i < ProgramSize; i++) {
+    for (int i = 0; i < ProgramSize; i++) 
+    {
         std::cout << std::hex << i << "  :" << std::hex << (int)MemorySpace[i] << std::endl;
     }
     #endif
 
-    #ifdef debug
 
 
-    #endif
+    while(true)
+    {
+        Fetch();
+        if(CurrentInstr==0)
+        {
+            break;
+        }
+        // Decode();
+        // Execute();
+        pc = pc + 4;
 
-    uint32_t CI;
-    uint32_t opcode;
-    uint32_t rd;
-    uint32_t funct3;
-    uint32_t rs1;
-    uint32_t imm;
+    }
 
-    // while (true) 
-    // {
-
-    //     Fetch();
-    //     if(CurrentInstr==0){
-    //         break;
-    //     }
-    //     Decode();
-    //     Execute();
-    //     pc += 4;
-    // }
-
-        // imm = CI >> 20;
-        // opcode = CI & 0x7f;
-        // std::cout << "Current Opcode: " << opcode << std::endl;
-
-        // rd = (CI >> 7) & 0x1f;
-        // funct3 = (CI >> 12) & 0x7;
-        // std::cout << "Current Funct3: " << funct3 << std::endl;
-        // rs1 = (CI >> 15) & 0x1f;
-
-        // switch (opcode) {
-        //     case 0x13:
-        //         if (funct3 == 0x0) {
-        //             std::cout << "Add Immediate detected" << std::endl;
-        //             x[rd] = x[rs1] + imm;
-        //             std::cout<<"x["<<rd<<"]"<<x[rd]<<std::endl;
-        //         }
-        //         break;
-
-        //     case 0x3:
-        //         if (funct3 == 0x2) {
-        //             std::cout << "Load word instruction detected" << std::endl;
-        //             x[rd] = MemorySpace[x[rs1]] | (MemorySpace[x[rs1] + 1] << 8) | (MemorySpace[x[rs1] + 2] << 16) | (MemorySpace[x[rs1] + 3] << 24);
-        //             std::cout<<"x["<<rd<<"]"<<x[rd]<<std::endl;
-        //         }
-        //         else if (funct3 == 0x0) {
-        //             std::cout << "Load byte instruction detected" << std::endl;
-        //             if (MemorySpace[x[rs1]]<<7 ) {
-        //                 x[rd] = MemorySpace[x[rs1]] | 0xFFFFFF00;
-        //             } else {
-        //                 x[rd] = MemorySpace[x[rs1]];
-        //             }
-        //             std::cout<<"x["<<rd<<"]"<<x[rd]<<std::endl;
-        //         }
-        //         else if (funct3 == 0x1) {
-        //             std::cout << "Load half word instruction detected" << std::endl;
-        //             if (MemorySpace[x[rs1] + 1] <<7) {
-        //                 x[rd] = (MemorySpace[x[rs1] + 1] << 8) | MemorySpace[x[rs1]] | 0xFFFF0000;
-        //             } else {
-        //                 x[rd] = (MemorySpace[x[rs1] + 1] << 8) | MemorySpace[x[rs1]];
-        //             }
-        //             std::cout<<"x["<<rd<<"]"<<x[rd]<<std::endl;
-        //         }
-        //         else if (funct3 == 0x4) {
-        //             std::cout << "Load half word unsigned instruction detected" << std::endl;
-        //             x[rd] = (MemorySpace[x[rs1] + 1] << 8) | MemorySpace[x[rs1]];
-        //             std::cout<<"x["<<rd<<"]"<<x[rd]<<std::endl;
-        //         }
-        //         else if (funct3 == 0x5) {
-        //             std::cout << "Load byte unsigned instruction detected" << std::endl;
-        //             x[rd] = MemorySpace[x[rs1]];
-        //             std::cout<<"x["<<rd<<"]"<<x[rd]<<std::endl;
-        //         }
-        //         break;
-            
-        //     case 0x23: std::cout<<"Store Instruction detected" <<std::endl;
-        //                if(funct3==0x2)
-        //                {
-                        
-        //                 std::cout<<"Store word Instruction detected" <<std::endl;
-        //                 MemorySpace[x[rd]]=x[rs1];
-        //                 MemorySpace[x[rd]+1]=x[rs1]<<8;
-        //                 MemorySpace[x[rd]+2]=x[rs1]<<16;
-        //                 MemorySpace[x[rd]+3]=x[rs1]<<24;
-        //                 std::cout<<"x[rs1]="<<std::dec<<x[rs1]<<std::endl;
-        //                }
-        //                break;
-
-
-        // }
-
-    // std::cout<<"MemorySpace[1000]="<<std::hex<<static_cast<int>(MemorySpace[1000])<<"\n";
-    // std::cout<<"MemorySpace[1001]="<<std::hex<<static_cast<int>(MemorySpace[1001])<<"\n";
-    // std::cout<<"MemorySpace[1002]="<<std::hex<<static_cast<int>(MemorySpace[1002])<<"\n";
-    // std::cout<<"MemorySpace[1003]="<<std::hex<<static_cast<int>(MemorySpace[1003])<<"\n";
-
-
-    // std::cout<<"x[11]="<<std::dec<<x[11]<<std::endl;
 }
